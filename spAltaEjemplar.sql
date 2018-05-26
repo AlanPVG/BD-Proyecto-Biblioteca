2d5df5c019df389bd279fd8fbce76de65cf43052
AlanPVG <alan.varela.garcia@gmail.com>
2018-05-26 18:06:50 -0500

    Create spAltaEjemplar.sql

---
+++ spAltaEjemplar.sql
@@ -0,0 +1,17 @@
+--Procedimiento para dar de alta un ejemplar
+
+
+CREATE OR REPLACE PROCEDURE spAltaEjemplar(
+	vIdMat ejemplar.idMaterial%TYPE,
+	vEstatus ejemplar.estatus%TYPE
+)
+AS
+BEGIN
+	INSERT INTO ejemplar
+	VALUES (vIdMat,num_Ejemplar.nextval,vEstatus);
+	
+	COMMIT;	
+
+END spAltaEjemplar;
+/
+SHOW ERRORS
