91a7ccbb14426818584dfe428898008c801d44f3
AlanPVG <alan.varela.garcia@gmail.com>
2018-05-27 11:51:39 -0500

    Create tgInsertaLibro.sql

---
+++ tgInsertaLibro.sql
@@ -0,0 +1,22 @@
+--Trigger que verifica que el material a insertar en la relacion libro sea un libro
+
+CREATE OR REPLACE TRIGGER tgInsertaLibro
+BEFORE INSERT ON libro
+FOR EACH ROW
+
+DECLARE
+
+
+BEGIN
+      
+      SELECT tipoMaterial
+      INTO vTipoMat
+      FROM tipoMaterial
+      WHERE idMaterial=:NEW.idMaterial;
+
+      IF vTipoMat != 'libro' THEN
+          RAISE_APPLICATION_ERROR(-20021,'El material que desea registrar no es un libro');
+      END IF;
+END tgInsertaLibro;
+/
+SHOW ERRORS
