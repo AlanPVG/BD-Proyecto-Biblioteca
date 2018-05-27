c657a845ffd26e7d97d5a579546c24b5f3dd7e2b
AlanPVG <alan.varela.garcia@gmail.com>
2018-05-26 23:12:55 -0500

    Update spAltaLibro.sql

--- spAltaLibro.sql
+++ spAltaLibro.sql
@@ -1,4 +1,18 @@
 --Procedimiento para dar de alta un libro
 
 CREATE OR REPLACE PROCEDURE spAltaLibro(
-      vIS
+      vIdMaterial IN libro.idMaterial%TYPE,
+      vNumAdqui IN libro.numAdqui%TYPE,
+      vISBN IN libro.isbn%TYPE,
+      vEdicion IN libro.edicion%TYPE
+)
+
+AS
+BEGIN
+
+      INSERT INTO libro
+      VALUES (vIdMaterial, vNumAdqui, vISBN, vEdicion);
+
+END spAltaLibro;
+/
+SHOW ERRORS
