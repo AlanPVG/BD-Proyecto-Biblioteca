b440e7515e69bb26b4253047394e6adb167be32b
AlanPVG <alan.varela.garcia@gmail.com>
2018-05-27 13:02:26 -0500

    Create spDevolucion.sql

---
+++ spDevolucion.sql
@@ -0,0 +1,39 @@
+--Procedimiento para la devolucion de un material en prestamo
+
+CREATE OR REPLACE PROCEDURE spDevolucion(
+      vIdMat IN prestamo.IdMaterial%TYPE,
+      vNumEjemp IN prestamo.numEjemplar%TYPE,
+      vFechaDev IN DATE     
+)
+
+AS
+      vFechaPrest prestamo.fechaPrestamo%TYPE;
+      vIdLect prestamo.idLector%TYPE;
+BEGIN
+
+      SELECT idLector
+      INTO vIdLect
+      FROM prestamo
+      WHERE idMaterial = vIdMat
+      AND numEjemplar = vNumEjemp;
+      
+      SELECT fechaPrestamo
+      INTO vFechaPrest
+      FROM prestamo
+      WHERE idMaterial = vIdMat
+      AND numEjemplar = vNumEjemp;
+      
+      INSERT INTO devuelveEjem(idLector,idMaterial,numEjemplar,fechaDevolucion)
+      VALUES(vIdLect, vIdMat, vNumEjemp, vFechaDev);
+          
+      DELETE FROM prestamo
+      WHERE idMaterial = vIdMaterial
+      AND numEjemplar = vNumEjemp;
+
+END spDevolucion;
+/
+SHOW ERRORS
+          
+          
+          
+      
