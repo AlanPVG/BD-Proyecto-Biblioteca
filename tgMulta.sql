49e1b99668c955275d0a92c3ceb6cf664a9b47cb
AlanPVG <alan.varela.garcia@gmail.com>
2018-05-28 17:56:44 -0500

    Update tgMulta.sql

--- tgMulta.sql
+++ tgMulta.sql
@@ -1,4 +1,41 @@
 --Trigger que genera multas en caso de ser necesario, al realizar la devolucion de un material en prestamo
+CREATE OR REPLACE FUNCTION ftDiasAtraso(
+	vIdLect IN prestamo.idLector%TYPE,
+	vIdMat IN prestamo.idMaterial%TYPE,
+	vNumEjemp IN prestamo.numEjemplar%TYPE)
+
+RETURN NUMBER
+IS
+	
+	vFechaPrest prestamo.fechaPrestamo%TYPE;
+      	vFechaDev devuelveEjem.fechaDevolucion%TYPE;
+	vDiasAtraso NUMBER;
+BEGIN
+      	SELECT fechaPrestamo
+      	INTO vFechaPrest
+      	FROM prestamo
+      	WHERE idLector = vIdLect
+      	AND idMaterial = vIdMat
+      	AND numEjemplar = vNumEjemp;
+      
+      	SELECT fechaDevolucion
+      	INTO vFechaDev
+      	FROM devuelveEjem
+      	WHERE idLector = vIdLect
+      	AND idMaterial = vIdMat
+      	AND numEjemplar = vNumEjemp;
+
+	SELECT (vFechaPrest - vFechaDev)
+	INTO vDiasAtraso
+	FROM DUAL;
+	
+	RETURN(vDiasAtraso);
+END ftDiasAtraso;
+/
+SHOW ERRORS
+
+
+
 
 CREATE OR REPLACE TRIGGER tgMulta
 BEFORE DELETE ON prestamo
@@ -11,37 +48,17 @@ DECLARE
       vMonto NUMBER;
       vDiasAtraso NUMBER;
 BEGIN
-      SELECT fechaPrestamo
-      INTO vFechaPrest
-      FROM prestamo
-      WHERE idLector = :OLD.idLector
-      AND idMaterial = :OLD.idMaterial
-      AND numEjemplar = :OLD.numEjemplar;
-      
-      SELECT fechaDevolucion
-      INTO vFechaDev
-      FROM devuelveEjem
-      WHERE idLector = :OLD.idLector
-      AND idMaterial = :OLD.idMaterial
-      AND numEjemplar = :OLD.numEjemplar;
-      
-      vMonto := 0;
-      vDiasAtraso := 0;
+      vDiasAtraso:=ftDiasAtraso(:OLD.idLector,:OLD.idMaterial,:OLD.numEjemplar);
+      vMonto := vDiasAtraso*10;
       
       UPDATE ejemplar
       SET estatus = 'disponible'
       WHERE idMaterial = :OLD.idMaterial
       AND numEjemplar = :OLD.numEjemplar;
-      
-      IF vFechaDev > vFechaPrest THEN
-            WHILE vFechaPrest < vFechaDev LOOP
-                  vMonto:=vMonto+10;
-                  vDiasAtraso := vDiasAtraso+1;
-                  vFechaPrest := vFechaPrest+1;
-            END LOOP;
+
             INSERT INTO multa (idMulta, idMaterial, numEjemplar, diasAtraso, monto, liquidado)
             VALUES (id_Multa.nextval, :OLD.idMaterial, :OLD.numEjemplar, vDiasAtraso, vMonto,'no');
-      END IF;
+
 END tgMulta;
 /
 SHOW ERRORS
