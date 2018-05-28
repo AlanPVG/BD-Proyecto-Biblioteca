73723f91860d42c35ea8952048de2f09aabaa873
AlanPVG <alan.varela.garcia@gmail.com>
2018-05-28 18:50:25 -0500

    Update tgMulta.sql

--- tgMulta.sql
+++ tgMulta.sql
@@ -1,41 +1,4 @@
 --Trigger que genera multas en caso de ser necesario, al realizar la devolucion de un material en prestamo
-CREATE OR REPLACE FUNCTION ftDiasAtraso(
-	vIdLect IN prestamo.idLector%TYPE,
-	vIdMat IN prestamo.idMaterial%TYPE,
-	vNumEjemp IN prestamo.numEjemplar%TYPE)
-
-RETURN NUMBER
-IS
-	
-	vFechaPrest prestamo.fechaPrestamo%TYPE;
-      	vFechaDev devuelveEjem.fechaDevolucion%TYPE;
-	vDiasAtraso NUMBER;
-BEGIN
-      	SELECT fechaPrestamo
-      	INTO vFechaPrest
-      	FROM prestamo
-      	WHERE idLector = vIdLect
-      	AND idMaterial = vIdMat
-      	AND numEjemplar = vNumEjemp;
-      
-      	SELECT fechaDevolucion
-      	INTO vFechaDev
-      	FROM devuelveEjem
-      	WHERE idLector = vIdLect
-      	AND idMaterial = vIdMat
-      	AND numEjemplar = vNumEjemp;
-
-	SELECT (vFechaPrest - vFechaDev)
-	INTO vDiasAtraso
-	FROM DUAL;
-	
-	RETURN(vDiasAtraso);
-END ftDiasAtraso;
-/
-SHOW ERRORS
-
-
-
 
 CREATE OR REPLACE TRIGGER tgMulta
 AFTER DELETE ON prestamo
@@ -48,8 +11,19 @@ DECLARE
       vMonto NUMBER;
       vDiasAtraso NUMBER;
 BEGIN
-      vDiasAtraso:=ftDiasAtraso(:OLD.idLector,:OLD.idMaterial,:OLD.numEjemplar);
-      vMonto := vDiasAtraso*10;
+      
+      	SELECT fechaDevolucion
+      	INTO vFechaDev
+      	FROM devuelveEjem
+      	WHERE idLector = vIdLect
+      	AND idMaterial = vIdMat
+      	AND numEjemplar = vNumEjemp;
+
+	SELECT (:OLD.fechaPrestamo - vFechaDev)
+	INTO vDiasAtraso
+	FROM DUAL;
+	
+      vMonto := (:OLD.fechaPrestamo - vFechaDev)*10;
       
       UPDATE ejemplar
       SET estatus = 'disponible'
