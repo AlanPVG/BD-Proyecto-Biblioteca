b6d5752645618fd3dee1ed30a5caceec698cb80d
AlanPVG <alan.varela.garcia@gmail.com>
2018-05-29 20:02:58 -0500

    Update spPagoMulta.sql

--- spPagoMulta.sql
+++ spPagoMulta.sql
@@ -7,7 +7,7 @@ CREATE OR REPLACE PROCEDURE spPagoMulta(
 
 AS
       vMonto multa.monto%TYPE;
-
+      vCambio NUMBER;
 BEGIN
 
     SELECT monto
@@ -15,10 +15,26 @@ BEGIN
     FROM multa
     WHERE idMulta=vIdMulta;
     
-    UPDATE multa
-    SET monto = vMonto - vPago
-    WHERE idMulta = vIdMulta;
+    IF vPago < vMonto THEN
+            UPDATE multa
+            SET monto = vMonto - vPago
+            WHERE idMulta = vIdMulta;
+            
+    ELSIF vPago = vMonto THEN
+            UPDATE multa
+            SET monto = vMonto - vPago, liquidado ='si'
+            WHERE idMulta = vIdMulta;
+            
+    ELSIF vPago > vMonto THEN
+           UPDATE multa
+           SET monto = 0
+           WHERE idMulta = vIdMulta;
+           
+           vCambio := vPago-vMonto;
+           DBMS_OUTPUT.PUT_LINE('Cambio = '||vCambio);
+    END IF;
     
+    COMMIT;
 END spPagoMulta;
 /
 SHOW ERRORS
