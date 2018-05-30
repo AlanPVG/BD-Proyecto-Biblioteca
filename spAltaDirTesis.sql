aa59088d4871633d0bead4719370af4f11942a5d
BraulioAO <braulioe697@gmail.com>
2018-05-29 23:38:20 -0500

    Datos tablas

--- spAltaDirTesis.sql
+++ spAltaDirTesis.sql
@@ -1,23 +1,17 @@
---Procedimiento para dar de alta director de tésis
-
-CREATE OR REPLACE PROCEDURE spAltaDirTesis(
-    vNomDir IN directorTesis.nomDirect%TYPE,
-    vApPatDir IN directorTesis.apPatDirect%TYPE,
-    vApMatDir IN directorTesis.apMatDirect%TYPE,
-    vGradoAcad IN directorTesis.gradoAcad%TYPE
-)
-
-AS
-BEGIN
-    
-     INSERT INTO directorTesis
-<<<<<<< HEAD
-     VALUES(id_Director.nextval-1, vNomDir, vApPatDir, vApMatDir, vGradoAcad);
-=======
-     VALUES(id_Director.nextval, vNomDir, vApPatDir, vApMatDir, vGradoAcad);
-     COMMIT;
->>>>>>> 9856d590fcc95d8905903afb0d8af23f16417e1a
+ --Procedimiento para dar de alta director de tésis
+ 
+ CREATE OR REPLACE PROCEDURE spAltaDirTesis(
+     vNomDir IN directorTesis.nomDirect%TYPE,
+     vApPatDir IN directorTesis.apPatDirect%TYPE,
+     vApMatDir IN directorTesis.apMatDirect%TYPE,
+     vGradoAcad IN directorTesis.gradoAcad%TYPE
+ )
+ 
+ AS
+ BEGIN
      
-END spAltaDirTesis;
-/
-SHOW ERRORS
+      INSERT INTO directorTesis
+      VALUES(id_Director.nextval-1, vNomDir, vApPatDir, vApMatDir, vGradoAcad);
+      
+ END spAltaDirTesis;
+ /
