929a92da679d2c640e42c0bd9cba45f485df6f5b
AlanPVG <alan.varela.garcia@gmail.com>
2018-05-29 22:11:13 -0500

    Update spPagoMulta.sql

--- spPagoMulta.sql
+++ spPagoMulta.sql
@@ -8,6 +8,7 @@ CREATE OR REPLACE PROCEDURE spPagoMulta(
 AS
       vMonto multa.monto%TYPE;
       vCambio NUMBER;
+      vIdLect lector.idLector%TYPE;
 BEGIN
 
     SELECT monto
@@ -15,20 +16,39 @@ BEGIN
     FROM multa
     WHERE idMulta=vIdMulta;
     
+    SELECT l.idLector
+    INTO vIdLect
+    FROM lector l
+    JOIN multa m
+    ON l.idLector = m.idLector
+    WHERE idMulta = vIdMulta;
+    
     IF vPago < vMonto THEN
             UPDATE multa
             SET monto = vMonto - vPago
             WHERE idMulta = vIdMulta;
+            
+            UPDATE lector
+            SET adeudo = vMonto - vPago
+            WHERE idLector=vIdLect;
             
     ELSIF vPago = vMonto THEN
             UPDATE multa
             SET monto = vMonto - vPago, liquidado = 'si'
             WHERE idMulta = vIdMulta;
             
+            UPDATE lector
+            SET adeudo = 0
+            WHERE idLector=vIdLect;
+            
     ELSIF vPago > vMonto THEN
            UPDATE multa
            SET monto = 0, liquidado = 'si'
            WHERE idMulta = vIdMulta;
+           
+           UPDATE lector
+           SET adeudo = 0
+           WHERE idLector=vIdLect;
            
            vCambio := vPago-vMonto;
            DBMS_OUTPUT.PUT_LINE('Cambio = '||vCambio);
