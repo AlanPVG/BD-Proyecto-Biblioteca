49f2a88039a2280af61989e16df90805879e6ec5
AlanPVG <alan.varela.garcia@gmail.com>
2018-05-29 19:28:51 -0500

    Create spPagoMulta.sql

---
+++ spPagoMulta.sql
@@ -0,0 +1,24 @@
+--Procedimiento para realizar pago de una multa
+
+CREATE OR REPLACE spPagoMulta(
+      vIdMulta IN multa.idMulta%TYPE,
+      vPago IN NUMBER
+)
+
+AS
+      vMonto multa.monto%TYPE;
+
+BEGIN
+
+    SELECT monto
+    INTO vMonto
+    FROM multa
+    WHERE idMulta=vIdMulta;
+    
+    UPDATE multa
+    SET monto = vMonto - vPago
+    WHERE idMulta = vIdMulta;
+    
+END spPagoMulta;
+/
+SHOW ERRORS
